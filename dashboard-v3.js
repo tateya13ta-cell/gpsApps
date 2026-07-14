b16546b0f47a90af3e863cce891db6542b60b15f
tateya13ta-cell <tateya13ta@gmail.com>
2026-07-14 23:07:03 +0700

    Redesign dashboard to approved premium layout

---
+++ dashboard-v3.js
@@ -0,0 +1,5 @@
+(function(){
+  const oldSwitchPage=switchPage;
+  pageMeta.dashboard=['ภาพรวมธุรกิจและการตั้งราคา','เครื่องมือสำรวจคู่แข่ง วิเคราะห์ต้นทุน และแนะนำราคา'];
+
+  const thaiMonths=['มกราคม','กุมภาพันธ์','มีนาค�
